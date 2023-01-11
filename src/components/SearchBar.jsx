73954b0e689bfc64ad1021ff69ef4e2642aaf41e
SergioAi <53959551+Spai26@users.noreply.github.com>
2023-01-11 10:59:28 -0500

    install router-dom

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,19 +1,28 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 
 const Searchbar = ({ onSearch }) => {
   const [value, setValue] = useState("");
+  const inputFocus = useRef(null);
+
+  /*hacer focus cuando al iniciar el componente */
+  useEffect(() => {
+    inputFocus.current.focus();
+  }, []);
 
   const handleOnChange = (e) => {
     setValue(e.target.value);
   };
 
   const handleSubmit = (e) => {
+    /*evitar que recarge solo funciona en los from*/
     e.preventDefault();
+    /*limpiar el input*/
     if (Object.keys(value).length) {
       e.target.reset();
       console.log("Add exitoso");
     }
   };
+
   return (
     <div>
       <form onSubmit={handleSubmit}>
@@ -22,7 +31,8 @@ const Searchbar = ({ onSearch }) => {
           name={value}
           placeholder="digita un ID valido"
           onChange={handleOnChange}
-          
+          ref={inputFocus}
+          autoFocus
         />
         <button
           onClick={() => {
