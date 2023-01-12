f52e22a75c5513a3ec7c9e297d6f7671bf995afd
SergioAi <53959551+Spai26@users.noreply.github.com>
2023-01-12 16:41:54 -0500

    adding routing and link to nav

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import Cards from "./components/Cards";
 import Nav from "./components/Nav";
+import About from "./components/pages/About";
+import Detail from "./components/pages/Detail.jsx";
+import LoginForm from "./components/Aplication Form/LoginForm";
 import { useState } from "react";
-
+import { Routes, Route } from "react-router-dom";
+import Error404 from "./components/pages/Error404";
 function App() {
   //home  => app
-
   /*
    * Estado para el el arr de personajes
    */
@@ -52,7 +55,17 @@ function App() {
     <div className="App">
       <Nav onSearch={onSearch} />
 
-      <Cards characters={characters} onClose={onClose} />
+      <Routes>
+        <Route exact path="/" element={<LoginForm />} />
+        <Route
+          exact
+          path="/home"
+          element={<Cards characters={characters} onClose={onClose} />}
+        />
+        <Route exact path="/about" element={<About />} />
+        <Route exact path="/detail/:id" element={<Detail />} />
+        <Route path="*" element={<Error404 />} />
+      </Routes>
     </div>
   );
 }
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,9 +1,11 @@
 import SearchBar from "./SearchBar";
-
+import { Link } from "react-router-dom";
 const Nav = ({ onSearch }) => {
   return (
     <div>
       <SearchBar onSearch={onSearch} />
+      <Link to="/home"> Home </Link>
+      <Link to="/about"> About </Link>
     </div>
   );
 };
