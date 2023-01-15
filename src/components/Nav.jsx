a8a2050ab93f5ca2aa06e8c24ca941b0d6af3150
SergioAi <53959551+Spai26@users.noreply.github.com>
2023-01-14 21:42:58 -0500

    adding styles for movile

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,22 +1,70 @@
 import SearchBar from "./SearchBar";
-import { Link } from "react-router-dom";
-
+import { NavLink, Link } from "react-router-dom";
+import { FaBars, FaTimes, FaSearch } from "react-icons/fa";
+import styled from "./Nav.module.css";
+import { Fragment, useState } from "react";
 const Nav = ({ onSearch, logOut }) => {
+  const [click, setClick] = useState(false);
+  const [search, setSearch] = useState(false);
+
+  const onChangeclick = () => {
+    setClick(!click);
+  };
+
+  const onSearchClick = () => {
+    setSearch(!search);
+  };
+
   return (
-    <div>
-      <div>
-        <SearchBar onSearch={onSearch} />
-        <Link to="/home"> Home </Link>
-        <Link to="/about"> About </Link>
+    <Fragment>
+      <div className={styled.nav__header}>
+        <div className={styled.nav__content}>
+          <div className={styled.nav__logo}>
+            <Link to="/home">
+              <span>SergioAi</span>
+            </Link>
+          </div>
+
+          <div className={styled.nav__hamburger}>
+            <div
+              className={styled.search}
+              onClick={onSearchClick}
+              click={search.toString()}
+            >
+              <div>
+                <FaSearch />
+              </div>
+            </div>
+            <div onClick={onChangeclick} click={click.toString()}>
+              {click ? <FaTimes /> : <FaBars />}
+            </div>
+          </div>
+
+          <div
+            className={`${styled.nav__links} 
+            ${click ? styled.isOpen : ""}
+          `}
+            onClick={onChangeclick}
+          >
+            <NavLink to="/home"> Home </NavLink>
+            <NavLink to="/about"> About </NavLink>
+          </div>
+
+          <div className={styled.nav__logout}>
+            <button
+              onClick={() => {
+                logOut();
+              }}
+            >
+              Log Out
+            </button>
+          </div>
+        </div>
       </div>
-      <button
-        onClick={() => {
-          logOut();
-        }}
-      >
-        Log Out
-      </button>
-    </div>
+      <div className={` ${search ? styled.isSearch : styled.nav__search}`}>
+        <SearchBar onSearch={onSearch} />
+      </div>
+    </Fragment>
   );
 };
 
