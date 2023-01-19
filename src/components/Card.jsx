f1a45f5f8eeee19bbc6305e9dd2a070b345edf9f
SergioAi <53959551+Spai26@users.noreply.github.com>
2023-01-19 11:54:29 -0500

    add button favorites into Card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,48 @@
 import { Link } from "react-router-dom";
 import styled from "./Card.module.css";
-
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+import {
+  addFavoriteCharacter,
+  deleteFavoriteCharacter,
+} from "../redux/actions/actions";
 
 const Card = ({ name, species, gender, image, onClose, id }) => {
+  const [isFav, setIsFav] = useState(false);
+  const myFavorites = useSelector((state) => state.myFavorites);
+  const dispath = useDispatch();
+
+  const handleFavorite = (id) => {
+    if (isFav) {
+      setIsFav(false);
+      dispath(deleteFavoriteCharacter(id));
+    } else {
+      setIsFav(true);
+      dispath(addFavoriteCharacter(id));
+    }
+  };
+
+  useEffect(() => {
+    myFavorites.forEach((fav) => {
+      if (fav.id === id) {
+        setIsFav(true);
+      }
+    });
+  }, [myFavorites]);
+
   return (
     <div className={styled.card}>
       <div className={styled.card_close}>
+        {isFav ? (
+          <button
+            className={styled.favorite}
+            onClick={() => handleFavorite(id)}
+          >
+            ❤️
+          </button>
+        ) : (
+          <button onClick={() => handleFavorite(id)}>🤍</button>
+        )}
         <button onClick={() => onClose(id)}>X</button>
       </div>
       <div className={styled.card_content}>
